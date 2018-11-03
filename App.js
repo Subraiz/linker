5c103a1227c473582fe3846f7c40898c108cc161
Subraiz <subraizahmed@gmail.com>
2018-11-03 14:18:37 -0400

    Added Libraries

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Text, View, SafeAreaView } from "react-native";
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <SafeAreaView>
         <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      </SafeAreaView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
