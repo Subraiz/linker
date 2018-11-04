4c23a0f9d1f5a53527c74538d7b191d4b449765a
David Shen <me@pantherman594.com>
2018-11-04 00:01:03 -0400

    Swipe deck fake queries

---
+++ src/components/Deck.js
@@ -0,0 +1,178 @@
+import React, { Component } from "react";
+import {
+  View,
+  Animated,
+  PanResponder,
+  Dimensions,
+  LayoutAnimation,
+  UIManager
+} from "react-native";
+
+const screeWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+const swipeThreshold = 0.32 * screeWidth;
+const swipeOutDuration = 400;
+
+class Deck extends Component {
+  constructor(props) {
+    super(props);
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (e, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: (e, gesture) => {
+        if (gesture.dx > swipeThreshold) {
+          this.forceSwipe("right");
+        } else if (gesture.dx < -swipeThreshold) {
+          this.forceSwipe("left");
+        } else {
+          this.resetPosition();
+        }
+      }
+    });
+
+    this.state = { panResponder, position, };
+  }
+
+  componentWillUpdate() {
+    UIManager.setLayoutAnimationEnabledExperimental &&
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    LayoutAnimation.spring();
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
+  forceSwipe(direction) {
+    const directionWeight = direction === "right" ? 1.5 : -1.5;
+    Animated.timing(this.state.position, {
+      toValue: { x: directionWeight * screeWidth, y: 0 },
+      duration: swipeOutDuration
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[0];
+    direction === "right" ? onSwipeRight(item) : onSwipeLeft(item);
+    this.state.position.setValue({ x: 0, y: 0 });
+  }
+
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-screeWidth * 2.5, 0, screeWidth * 2.5],
+      outputRange: ["-120deg", "0deg", "120deg"]
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate: rotate }]
+    };
+  }
+
+  renderCards() {
+    console.log("render");
+    console.log(this.props.data.length);
+    if (this.props.data.length === 0) {
+      return (
+        <Animated.View style={styles.cardStyle}>
+          {this.props.renderNoMoreCards()}
+        </Animated.View>
+      );
+    }
+
+    if (this.props.data.length === 1) {
+      return [
+        (
+          <Animated.View style={styles.cardStyle}>
+            {this.props.renderNoMoreCards()}
+          </Animated.View>
+        ), (
+          <Animated.View
+            key={this.props.data[0].uid}
+            style={[this.getCardStyle(), styles.cardStyle]}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(this.props.data[0])}
+          </Animated.View>
+        )
+      ];
+    }
+
+    console.log("long");
+    return this.props.data
+      .map((candidate, i) => {
+        if (i === 0) {
+          return (
+            <Animated.View
+              key={candidate.uid}
+              style={[this.getCardStyle(), styles.cardStyle]}
+              {...this.state.panResponder.panHandlers}
+            >
+              {this.props.renderCard(candidate)}
+            </Animated.View>
+          );
+        }
+        if (i === 1) {
+          // Return Animated.View for rendering purposed
+          return (
+            <Animated.View
+              key={candidate.id}
+              style={[styles.cardStyle, styles.nextCardStyle]}
+            >
+              {this.props.renderCard(candidate)}
+            </Animated.View>
+          );
+        }
+      }).reverse();
+  }
+  render() {
+    return (
+      <View>
+        <View style={styles.deckContainer}>{this.renderCards()}</View>
+      </View>
+    );
+  }
+}
+
+Deck.defaultProps = {
+  onSwipeLeft: () => {},
+  onSwipeRight: () => {},
+  renderNoMoreCards: () => {}
+};
+
+const styles = {
+  deckContainer: {
+    display: "flex",
+    alignItems: "center"
+  },
+  cardStyle: {
+    position: "absolute",
+    width: screeWidth
+  },
+  nextCardStyle: {
+    selfAlign: "center",
+    width: screeWidth - 10,
+    top: screenHeight / 90,
+    opacity: 0.95
+  },
+  likeButton: {
+    display: "flex",
+    alignItems: "center",
+    height: screenHeight * 0.9,
+    width: screeWidth
+  },
+  dislikeButton: {
+    position: "absolute",
+    width: screeWidth * 0.25,
+    height: screenHeight * 0.9
+  }
+};
+
+export default Deck;
--- src/components/SwipeView.js
+++ src/components/SwipeView.js
@@ -1,30 +1,147 @@
 import React, { Component } from "react";
-import { Switch } from "react-native";
+import { Card, Button } from "react-native-elements";
+import Deck from "./Deck";
+
 import { Actions } from "react-native-router-flux";
 import { Text, View, TouchableOpacity } from "../common/shared-components";
 import { Database } from "../models/Database";
+import md5 from "react-native-md5";
 
 export default class SwipeView extends Component {
   constructor(props) {
     super(props);
     this.state = {
       user: {},
+      candidates: [],
     };
+
+    if (props.userType && props.userType == USER.STUDENT) {
+      this.getNextCandidate = () => {
+        return Database.getNextRecruiter();
+      };
+      this.renderNextCard = (candidate) => {
+        return this.renderRecruiterCard(candidate);
+      };
+    } else {
+      this.getNextCandidate = () => {
+        return Database.getNextStudent();
+      };
+      this.renderNextCard = (candidate) => {
+        return this.renderStudentCard(candidate);
+      };
+    }
+
+    this.renderNextCard = this.renderNextCard.bind(this);
+    this.getNextCandidate = this.getNextCandidate.bind(this);
+    this.setNextCandidate = this.setNextCandidate.bind(this);
+
+    this.updateList = this.updateList.bind(this);
+    this.onSwipeLeft = this.onSwipeLeft.bind(this);
+    this.onSwipeRight = this.onSwipeRight.bind(this);
+  }
+
+  setNextCandidate(candidate) {
+    let { candidates } = this.state.candidates;
+    candidates.push(candidate);
+    this.setState({ candidates });
   }
 
   componentDidMount() {
-    const nextStudent = Database.getNextStudent();
-    this.setState({ user: nextStudent });
+    let { candidates } = this.state;
+    for (let i = 0; i < 5; i++) {
+      const nextCandidate = this.getNextCandidate();
+      if (!nextCandidate) {
+        // render last card view
+      } else {
+        candidates.push(nextCandidate);
+      }
+    }
+    this.setState({ candidates });
+  }
+
+  onSwipeLeft() {
+    // TODO: update database
+    this.updateList();
+  }
+
+  onSwipeRight() {
+    // TODO: update database
+    this.updateList();
+  }
+
+  updateList() {
+    let { candidates } = this.state;
+    const nextCandidate = this.getNextCandidate();
+    if (!nextCandidate) {
+      // render last card view
+    } else {
+      candidates.push(nextCandidate);
+    }
+    candidates.shift();
+    this.setState({ candidates });
+  }
+
+  getGravatarUri(email) {
+    return "https://www.gravatar.com/avatar/" + md5.hex_md5(email.toLowerCase().trim()) + "?s=300";
+  }
+
+  renderStudentCard(candidate) {
+    return (
+      <Card
+        key={candidate.uid}
+        title={candidate.name}
+        image={{ uri: this.getGravatarUri(candidate.email) }}
+        imageStyle={{ height: 400 }}
+      >
+        <Text style={{ marginBottom: 10 }}>
+          School: {candidate.school}
+          Major: {candidate.major}
+        </Text>
+        <Button
+          icon={{ name: "assignment" }}
+          backgroundColor="#03A9F4"
+          title="View More"
+        />
+      </Card>
+    );
+  }
+
+  renderNoMoreCards() {
+    console.log("All done");
+    return (
+      <Card title={"All Done"}>
+        <Button
+          icon={{ name: "assignment" }}
+          backgroundColor="#03A9F4"
+          title="Get More"
+        />
+      </Card>
+    );
+  }
+
+  renderRecruiterCard(candidate) {
+    return (
+      <Text>{candidate.name}</Text>
+    );
   }
 
   render() {
-    const { user } = this.state;
+    const { user, candidates } = this.state;
     console.log(user);
     return (
-      <View>
-        <Text>{user.name}</Text>
-        <Text>{user.email}</Text>
+      <View style={styles.container}>
+        <Deck
+          data={candidates}
+          renderCard={this.renderNextCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+          onSwipeLeft={this.onSwipeLeft}
+          onSwipeRight={this.onSwipeRight}
+        />
       </View>
     );
   }
 }
+
+const styles = {
+
+};
